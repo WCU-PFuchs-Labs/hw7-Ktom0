0a904a44a07b83ba6d9930cfbe7bff08ba851957
KevinT <98439580+Ktom0@users.noreply.github.com>
2025-10-28 23:32:50 -0400

    Update Divide.java

--- binary/Divide.java
+++ binary/Divide.java
@@ -1,10 +1,30 @@
 package binary;
-import java.util.Random; 
 
 public class Divide extends Binop {
+
+    private static final double EPS = 0.0001;
+
     @Override
     public double eval(double left, double right) {
-        return left / right;
+        double denominator = right;
+
+        double magnitude = Math.abs(denominator);
+        boolean tooSmall = (magnitude < EPS);
+
+        if (tooSmall) {
+            double safeFallback = 1.0;
+            return safeFallback;
+        } 
+        else {
+            double quotient = left / denominator;
+            
+            if (Double.isInfinite(quotient)) {
+                return 1.0;
+            } 
+            else {
+                return quotient;
+            }
+        }
     }
 
     @Override
